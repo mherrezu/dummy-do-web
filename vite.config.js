8d49fe245992366ede4023e89d5924e26b2539e2
mherrezu <114182402+mherrezu@users.noreply.github.com>
2024-04-28 00:32:05 +0200

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://mherrezu.github.io/dummy-do-web',
+  base: 'https://mherrezu.github.io/',
   plugins: [
     vue(),
   ],
